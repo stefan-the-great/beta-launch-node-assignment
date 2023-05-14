5e358641d8abbfbc58c963d22d2972e6919f8c74
grimreaper0112 <stefanfer01@gmail.com>
2023-05-14 20:10:27 +0530

    Top Menu is done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Menu from './components/Menu';
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Menu/>
     </div>
   );
 }
---
+++ src/components/Menu.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Menu() {
+  return (
+    <div className='menu'>
+        <h1>People</h1>
+    </div>
+  )
+}
+
+export default Menu
